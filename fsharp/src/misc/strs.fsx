8a0b5911b1f54100664c2e974894b2910fd398f1
andrii <quadr988@gmail.com>
2022-06-15 21:34:09 +0300

    d

--- fsharp/src/misc/strs.fsx
+++ fsharp/src/misc/strs.fsx
@@ -11,3 +11,11 @@ let bytes = "bytesbytesbytes"B
 let xA = 0xFFy
 let xB = 0o7777un
 let xC = 0b10010UL
+
+let name = "Petro"
+printfn "%s" name[0..2]
+
+let printChar (str : string) (index : int) =
+    printfn "First character: %c" (str.Chars(index))
+
+printChar "Vasylko" 3    
